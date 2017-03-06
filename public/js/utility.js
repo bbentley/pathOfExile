eca97a8ff087062949fe28c22e57d784aad97c8f
bbentley <brian.matthew.bentley@gmail.com>
2017-03-06 16:22:56 -0600

    Initail commit

---
+++ public/js/utility.js
@@ -0,0 +1,25 @@
+const sheetToJSON = () => {
+// ID of the Google Spreadsheet
+//  const spreadsheetID = "SPREADSHEET KEY";
+const spreadsheetID = "1XG0Eg4JPN5DV8pfbwEzQp2gCVuKQmORZmtaBuh-R3XA";
+// https://docs.google.com/spreadsheets/d/1ypN0qvA2GFqTHwgXRXaYoxKHFd0MsDs3bFXUFQ1xHH0/pubhtml
+
+// https://docs.google.com/spreadsheets/d/1XG0Eg4JPN5DV8pfbwEzQp2gCVuKQmORZmtaBuh-R3XA/pubhtml
+// Make sure it is public or set to Anyone with link can view
+// Replaced od6/public with 1/public
+const url = 'https://spreadsheets.google.com/feeds/list/' + spreadsheetID + '/1/public/values?alt=json';
+console.log(url);
+$.getJSON(url, data => {
+
+const entry = data.feed.entry;
+console.log(entry);
+
+$(entry).each(
+() => {
+$('.results').prepend('<h2>'+this.gsx$name.$t+'</h2><p>'+this.gsx$alias+'</p><p>'+this.gsx$cost+'</p><p>'+this.gsx$type+'</p>');
+});
+
+});
+};
+
+//http://spreadsheets.google.com/feeds/list/o13394135408524254648.240766968415752635/od6/public/values
